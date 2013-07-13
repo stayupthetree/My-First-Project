1ba5ffd507d2021ef7b3b8e31af6241cabb8f8fd
Fahim Farook <fahimf@gmail.com>
2013-07-13 10:16:33 +0800

    Initial Commit

---
+++ My First Project/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  My First Project
+//
+//  Created by Fahim Farook on 13/7/13.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+	@autoreleasepool {
+	    
+	    // insert code here...
+	    NSLog(@"Hello, World!");
+	    
+	}
+    return 0;
+}
+
